5a1cd78607564f086f7ae3978770a26ff4a0aba6
kartik3yaS <kartikeyashukla009@gmail.com>
2024-05-16 14:59:19 +0530

    updated and added sidebarNav items

--- app/problems/[id]/page.js
+++ app/problems/[id]/page.js
@@ -8,7 +8,7 @@ export default function ProblemId() {
     return(
         <div className="relative flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12">
             <div className="flex space-x-4 border border-white relative px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
-                Hello {params.id}
+                Hello {params.id} 
             </div>
         </div>
     )
--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -7,6 +7,7 @@ import { ViewVerticalIcon } from "@radix-ui/react-icons"
 import { Icons } from "@/components/icons"
 import { cn } from "@/lib/utils"
 import { Button } from "./ui/button"
+import { docsConfig } from "./config/docs"
 import { ScrollArea } from "./ui/scroll-area"
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 
@@ -60,37 +61,22 @@ export function MobileNav() {
             >
                 <Icons.logo className="mr-2 h-4 w-4" />
             </MobileLink>
-          {/* <ScrollArea className="my-4 h-[calc(100vh-8rem)] pb-10 pl-6">
-            <div className="flex flex-col space-y-2">
-              {docsConfig.sidebarNav.map((item, index) => (
-                <div key={index} className="flex flex-col space-y-3 pt-6">
-                  <h4 className="font-medium">{item.title}</h4>
-                  {item?.items?.length &&
-                    item.items.map((item) => (
-                      <React.Fragment key={item.href}>
-                        {!item.disabled &&
-                          (item.href ? (
-                            <MobileLink
-                              href={item.href}
-                              onOpenChange={setOpen}
-                              className="text-muted-foreground"
-                            >
-                              {item.title}
-                              {item.label && (
-                                <span className="ml-2 rounded-md bg-[#adfa1d] px-1.5 py-0.5 text-xs leading-none text-[#000000] no-underline group-hover:no-underline">
-                                  {item.label}
-                                </span>
-                              )}
-                            </MobileLink>
-                          ) : (
-                            item.title
-                          ))}
-                      </React.Fragment>
-                    ))}
-                </div>
-              ))}
-            </div>
-          </ScrollArea> */}
+            <ScrollArea className="my-4 h-[calc(100vh-8rem)] pb-10 pl-6">
+              <div className="flex flex-col space-y-3">
+                {docsConfig.mainNav?.map(
+                  (item) =>
+                    item.href && (
+                      <MobileLink
+                        key={item.href}
+                        href={item.href}
+                        onOpenChange={setOpen}
+                      >
+                        {item.title}
+                      </MobileLink>
+                    )
+                )}
+              </div>
+            </ScrollArea>
         </SheetContent>
       </Sheet>
     )
